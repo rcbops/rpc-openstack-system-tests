46f479a85a179d97a3eace06c018110efa5f070e
John Duarte <john.duarte@rackspace.com>
2018-02-23 15:21:36 -0800

    (ASC-144) Add script to execute tests
    This commit adds an `execute_test.sh` script to:
    
    1. Create a python virtualenv with the requirements and constraints
       defined in this repo.
    2. Iteratively run `molecule converge` and `molecule verify` on the
       submodules included in the `molecules` directory.
    3. Gather the test results into a tar file at the root directory of the
       repo.

---
+++ execute_tests.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+## Deploy virualenv for testing enironment molecule/ansible-playbook/infratest
+
+## Shell Opts ----------------------------------------------------------------
+
+set -ev
+set -o pipefail
+export ANSIBLE_HOST_KEY_CHECKING=False
+
+## Variables -----------------------------------------------------------------
+
+SYS_VENV_NAME="${SYS_VENV_NAME:-venv-molecule}"
+SYS_CONSTRAINTS="constraints.txt"
+SYS_REQUIREMENTS="requirements.txt"
+SYS_INVENTORY="${SYS_INVENTORY:-/opt/openstack-ansible/playbooks/inventory}"
+
+## Main ----------------------------------------------------------------------
+
+# Create virtualenv for molecule
+virtualenv --no-pip --no-setuptools --no-wheel "${SYS_VENV_NAME}"
+
+# Activate virtualenv
+source "${SYS_VENV_NAME}/bin/activate"
+
+# Ensure that correct pip version is installed
+PIP_TARGET="$(awk -F= '/^pip==/ {print $3}' ${SYS_CONSTRAINTS})"
+VENV_PYTHON="${SYS_VENV_NAME}/bin/python"
+VENV_PIP="${SYS_VENV_NAME}/bin/pip"
+
+if [[ "$(${VENV_PIP} --version)" != "pip ${PIP_TARGET}"* ]]; then
+    CURL_CMD="curl --silent --show-error --retry 5"
+    OUTPUT_FILE="get-pip.py"
+    ${CURL_CMD} https://bootstrap.pypa.io/get-pip.py > ${OUTPUT_FILE}  \
+        || ${CURL_CMD} https://raw.githubusercontent.com/pypa/get-pip/master/get-pip.py > ${OUTPUT_FILE}
+    GETPIP_OPTIONS="pip setuptools wheel --constraint ${SYS_CONSTRAINTS}"
+    ${VENV_PYTHON} ${OUTPUT_FILE} ${GETPIP_OPTIONS} \
+        || ${VENV_PYTHON} ${OUTPUT_FILE} --isolated ${GETPIP_OPTIONS}
+fi
+
+# Install test suite requirements
+PIP_OPTIONS="-c ${SYS_CONSTRAINTS} -r ${SYS_REQUIREMENTS}"
+${VENV_PIP} install ${PIP_OPTIONS} || ${VENV_PIP} install --isolated ${PIP_OPTIONS}
+
+# Generate moleculerized inventory from openstack-ansible dynamic inventory
+${SYS_INVENTORY}/dynamic_inventory.py > dynamic_inventory.json
+
+# Run molecule converge and verify
+# for each submodule in ${SYS_TEST_SOURCE}/molecules
+set +e # allow test stages to return errors
+for TEST in molecules/* ; do
+    ./moleculerize.py --output "$TEST/molecule/default/molecule.yml" /tmp/dynamic_inventory.json
+    pushd "$TEST"
+    molecule converge
+    molecule verify
+    popd
+done
+
+# Gather junit.xml results
+rm -f test_results.tar  # ensure any previous results are deleted
+ls  molecules/*/molecule/*/*.xml | tar -cvf test_results.tar --files-from=-
